d45d478b2116b9f377bda75c460ed62a081b1353
Max Mallett <maxdmallett@gmail.com>
2024-01-05 23:12:00 +0000

    Contact Links added to mobile nav

---
+++ app/components/mobileMenuIconLink.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+interface MobileMenuIconLinkProps {
+    href: string;
+    label: string;
+    icon: React.ReactNode;
+}
+
+const MobileMenuIconLink = (props: MobileMenuIconLinkProps) => {
+    const { href, label, icon } = props
+    return (
+        <a 
+            className="block hover:text-slate-200 p-4 bg-black/10 rounded-lg mr-3"
+            href={`${href}`}
+            target="_blank" 
+            rel="noreferrer noopener"
+            aria-label={`${label} (opens in a new tab)`}
+        >
+            {icon}
+        </a>
+    )
+}
+
+export default MobileMenuIconLink
+
--- app/components/nav/mobileNavMenu.tsx
+++ app/components/nav/mobileNavMenu.tsx
@@ -3,6 +3,11 @@ import Link from "next/link"
 import { navItems } from './navData';
 import { scrollToElement } from '../../helpers/scrollTo';
 import { handleRouteLinkClick } from '../../helpers/handleRouteLinkClick';
+import MobileMenuIconLink from '../mobileMenuIconLink';
+import { contactEmailAddress, gitHubLink, linkedInLink } from '../../data/links';
+import EmailIcon from '../icons/email';
+import LinkedInIcon from '../icons/linkedIn';
+import GitHubIcon from '../icons/github';
 
 interface MobileNavMenuProps {
     visible: boolean;
@@ -20,7 +25,7 @@ const MobileNavMenu = (props: MobileNavMenuProps) => {
 
     return (
         <div className={`
-            mobile-nav-menu fixed top-0 left-0 w-full h-screen bg-cyan-950 z-50
+            mobile-nav-menu fixed top-0 left-0 w-full h-screen bg-cyan-950 z-50 px-5
             ${visible ? 'open' : ''}
         `}>
             <button onClick={closeMenu} type="button" className="top-4 right-4 absolute p-2 inline-flex items-center justify-center text-white focus:outline-none focus:bg-transparent">
@@ -29,7 +34,7 @@ const MobileNavMenu = (props: MobileNavMenuProps) => {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
                 </svg>
             </button>
-            <ul className="items-start flex flex-col list-none mt-16 ms-5">
+            <ul className="items-start flex flex-col list-none mt-16">
                 {navItems.map((item, index) => (
                     <li key={index}>
                         <Link 
@@ -48,6 +53,25 @@ const MobileNavMenu = (props: MobileNavMenuProps) => {
                     </Link>
                 </li>
             </ul>
+            <hr className="h-px my-8 border-0 bg-white/5" />
+
+            <div className="flex justify-start mt-12">
+                <MobileMenuIconLink
+                    href={`mailto:${contactEmailAddress}`}
+                    label='Email'
+                    icon={<EmailIcon />}
+                />
+                <MobileMenuIconLink
+                    href={gitHubLink} 
+                    label='GitHub'
+                    icon={<GitHubIcon />}
+                />
+                <MobileMenuIconLink
+                    href={linkedInLink} 
+                    label='LinkedIn'
+                    icon={<LinkedInIcon />}
+                />
+            </div>
         </div>
     )
 }
