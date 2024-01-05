2353b00340cd690d8dfcc9362a42715403635ff7
Max Mallett <maxdmallett@gmail.com>
2024-01-05 23:32:43 +0000

    Experience timelines connect

--- app/components/home/job.tsx
+++ app/components/home/job.tsx
@@ -12,7 +12,7 @@ const Job = (props: IJobsProperties) => {
     const alignment: string = props.index % 2 === 0 ? 'left' : 'right'
 
     return (
-        <article className="relative flex mb-16 sm:mb-20 last:mb-0">
+        <article className="relative flex mb-16 sm:mb-2 last:mb-0 group">
 
             <div className='grid md:grid-cols-11'>
 
@@ -37,14 +37,14 @@ const Job = (props: IJobsProperties) => {
                     <p className='text-md text-white text-opacity-50 mb-4'>
                         {props.description}
                     </p>
-                    <div className={`${alignment === 'left' ? 'md:ms-auto' : ''}`}>
+                    <div className={`${alignment === 'left' ? 'md:ms-auto' : ''} sm:mb-20 group-last:sm:mb-1`}>
                         <PillList pills={props.skills}/>
                     </div>
                 </div>
 
                 <div className="hidden md:flex col-span-1 flex-col items-center pt-1.5">
                     <div className="w-4 h-4 flex-shrink-0 bg-teal-300 rounded-full mb-4"></div>
-                    <div className="w-0.5 h-full bg-white flex-grow-1"></div>
+                    <div className="w-0.5 h-full bg-white/20 flex-grow-1"></div>
                 </div>
                 
                 <p className={`hidden md:block col-span-5 text-gray-500 uppercase text-sm mt-1 
