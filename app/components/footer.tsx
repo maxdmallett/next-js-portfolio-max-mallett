19c93a27f5919a14377bfec5a9f3a8085dac6bd4
Max Mallett <maxdmallett@gmail.com>
2024-01-05 21:27:40 +0000

    Contact and footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,8 +1,8 @@
 
 const Footer = () => {
     return (
-        <footer className="flex text-center justify-center">
-            
+        <footer className="flex text-center justify-center py-5">
+            <p className="text-white/30 text-sm">&#169; {new Date().getFullYear()} Max Mallett</p> 
         </footer>
     )
 }
--- app/components/home/contact.tsx
+++ app/components/home/contact.tsx
@@ -3,48 +3,12 @@ import SectionHeading from "../sectionHeading"
 const Contact = () => {
     return (
         <section id="contact" className='py-12 sm:py-24'>
-            
             <SectionHeading title='Contact' />
-
             <div className="text-center max-w-2xl mx-auto">
-                
                 <p className="mb-8">
-                    Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.
+                    Get in touch by emailing <a href="mailto:maxdmallett@gmail.com" target="_blank" className='font-medium text-white hover:underline'>maxdmallett@gmail.com</a><br />or message me on <a href="https://www.linkedin.com/in/max-mallett-aa3434107/" target="_blank" className='font-medium text-white hover:underline'>LinkedIn</a>.
                 </p>
-                       
-                <form action="#" className="space-y-8 text-left">
-                    <div>
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-                        <input 
-                            type="email" 
-                            id="email" 
-                            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
-                            placeholder="john-doe@gmail.com"
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-                        <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required />
-                    </div>
-                    <div className="sm:col-span-2">
-                        <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-                        <textarea 
-                            id="message" 
-                            rows={6}
-                            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 resize-none"
-                            placeholder="Enter your message"
-                        >
-                        </textarea>
-                    </div>
-                    <button 
-                        type="submit" 
-                        className="inline-block rounded-md bg-transparent border border-teal-300 px-5 py-3 text-md font-normal text-teal-300 mx-auto">
-                        Send message
-                    </button>
-                </form>
-               
             </div>
-          
         </section>
     )
 }
