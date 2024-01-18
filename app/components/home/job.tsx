db525e767c1a7bfd8321eda18d6c06e490f2b448
Max Mallett <maxdmallett@gmail.com>
2024-01-18 22:43:16 +0000

    Animation added to experience section

--- app/components/home/job.tsx
+++ app/components/home/job.tsx
@@ -1,7 +1,10 @@
+'use client'
+
 import React from 'react'
 import PillList from '../pillList';
 import Link from 'next/link';
 import { IJob } from '../../data/jobs';
+import { motion } from 'framer-motion';
 
 interface IJobsProperties extends IJob {
     index: number;
@@ -16,10 +19,15 @@ const Job = (props: IJobsProperties) => {
 
             <div className='grid md:grid-cols-11'>
 
-                <div 
+                <motion.div
                     className={`md:col-span-5 flex flex-col
                     ${alignment === 'left' ? 'md:text-right' : 'text-left order-last' }`}
+                    initial={{ opacity: 0, y: 30 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: true }}
+                    transition={{ type: "spring", duration: 1, delay: 0 }}
                 >
+                   
                     <Link 
                         href={props.url}
                         target='_blank'
@@ -40,19 +48,31 @@ const Job = (props: IJobsProperties) => {
                     <div className={`${alignment === 'left' ? 'md:ms-auto' : ''} sm:mb-20 group-last:sm:mb-1`}>
                         <PillList pills={props.skills} alignment={alignment} />
                     </div>
-                </div>
+                    
+                </motion.div>
 
-                <div className="hidden md:flex col-span-1 flex-col items-center pt-1.5">
+                <motion.div
+                    className="hidden md:flex col-span-1 flex-col items-center pt-1.5"
+                    initial={{ opacity: 0, y: 30 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: true }}
+                    transition={{ type: "spring", duration: 1, delay: 0.2 }}
+                >
                     <div className="w-4 h-4 flex-shrink-0 bg-primary-300 rounded-full mb-4"></div>
                     <div className="w-0.5 h-full bg-white/20 flex-grow-1"></div>
-                </div>
+                </motion.div>
                 
-                <p className={`hidden md:block col-span-5 text-gray-500 uppercase text-sm mt-1 
+                <motion.p
+                    className={`hidden md:block col-span-5 text-gray-500 uppercase text-sm mt-1 
                     ${alignment === 'left' ? 'text-left' : 'text-right order-first' }`}
+                    initial={{ opacity: 0, y: 30 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: true }}
+                    transition={{ type: "spring", duration: 1, delay: 0.4 }}
                 >
                     {props.timeframe}
-                </p>
-
+                </motion.p>
+                    
             </div>
 
         </article>
