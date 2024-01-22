b37650ceeb0630804dd0ca25d710b053a4f6f401
Max Mallett <maxdmallett@gmail.com>
2024-01-22 22:41:09 +0000

    Fixed code snippet overflow on mobile

--- app/components/codeSnippet.tsx
+++ app/components/codeSnippet.tsx
@@ -9,7 +9,7 @@ const CodeSnippet = (props: Props) => {
     const { code, caption } = props;
     return (
         <figure className="block mb-12">
-            <pre className='bg-slate-800 text-slate-300 text-xs rounded-md shadow-sm px-8 py-6 whitespace-break-spaces'>
+            <pre className='bg-slate-800 text-slate-300 text-xs rounded-md shadow-sm px-8 py-6 overflow-x-auto'>
                 <code>{code}</code>            
             </pre>
             <figcaption className="mt-2 text-sm text-center text-gray-500 dark:text-gray-400">
