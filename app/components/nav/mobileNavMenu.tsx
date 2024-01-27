945647caeffd3da7ef752c5d1c8bd7786351dd53
Max Mallett <maxdmallett@gmail.com>
2024-01-27 20:38:54 +0000

    Removed unused code

--- app/components/nav/mobileNavMenu.tsx
+++ app/components/nav/mobileNavMenu.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 import { navItems } from './navData';
-import { scrollToElement } from '../../helpers/scrollTo';
 import { handleRouteLinkClick } from '../../helpers/handleRouteLinkClick';
 import MobileMenuIconLink from '../mobileMenuIconLink';
 import {
